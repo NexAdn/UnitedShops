e9617caadbb824353542b4b0240e073e2c88d6c7
NexAdn <adrianschollmeyer@web.de>
2015-11-04 15:43:48 +0100

    Added getter for Overview GUI

--- src/main/java/io/github/nexadn/unitedshops/command/ShopGUIHandler.java
+++ src/main/java/io/github/nexadn/unitedshops/command/ShopGUIHandler.java
@@ -3,11 +3,27 @@ package io.github.nexadn.unitedshops.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import io.github.nexadn.unitedshops.gui.GUIContainer;
 
 public class ShopGUIHandler implements CommandExecutor {
 
 	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] sArgv) {
-		
-		return false;
+		GUIContainer.initGUI();
+		if(commandSender instanceof Player) {
+			// Nur für Spieler
+			Player player = (Player)commandSender;
+			if(player.hasPermission("unitedshops.useshop")) {
+				player.openInventory(GUIContainer.getMenuGui()); // Menü GUI öffnen
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			commandSender.sendMessage("Du kannst dies nur als Spieler nutzen!");
+			return true;
+		}
+		// return false;
 	}
 }
