7c277b1273e23c2f4bd1785e4aa0ac84b0600150
NexAdn <adrianschollmeyer@web.de>
2015-11-05 16:48:45 +0100

    Added test command: /ushopdebug eco <take|give> <money>

--- src/main/java/io/github/nexadn/unitedshops/command/UShopDebug.java
+++ src/main/java/io/github/nexadn/unitedshops/command/UShopDebug.java
@@ -5,6 +5,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import net.milkbowl.vault.economy.Economy;
+
 public class UShopDebug implements CommandExecutor {
 
 	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] sArgv) {
@@ -20,6 +22,21 @@ public class UShopDebug implements CommandExecutor {
 				 * Argument 1: Debugbefehl
 				 * Argument 2-n: Debug Argumente
 				*/
+				if( sArgv[0].equalsIgnoreCase("eco") ) {
+					// Vault tests
+					Economy eco = null;
+					if( sArgv[1].equalsIgnoreCase("take") ) {
+						// Geld abziehen
+						double val = Double.parseDouble( sArgv[2] );
+						eco.withdrawPlayer(player, val);
+					} else if( sArgv[1].equalsIgnoreCase("give") ) {
+						// Geld geben
+						double val = Double.parseDouble( sArgv[2] );
+						eco.depositPlayer(player, val);
+					} else {
+						return false;
+					}
+				}
 				return true;
 			} else {
 				return false;
