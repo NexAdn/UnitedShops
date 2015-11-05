72e30284c4f0cb0623cf092ce4b8e4f973997d12
NexAdn <adrianschollmeyer@web.de>
2015-11-05 19:24:23 +0100

    Added code for onEnable() (initEco())

--- src/main/java/io/github/nexadn/unitedshops/UnitedShops.java
+++ src/main/java/io/github/nexadn/unitedshops/UnitedShops.java
@@ -1,7 +1,11 @@
 package io.github.nexadn.unitedshops;
 
+import java.util.logging.Level;
+
 import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import io.github.nexadn.unitedshops.tradeapi.EcoManager;
 
 public class UnitedShops extends JavaPlugin {
 	
@@ -11,6 +15,21 @@ public class UnitedShops extends JavaPlugin {
 	public void onEnable()
 	{
 		this.server = getServer();
+		
+		try 
+		{
+			Thread.sleep(5000);
+		} 
+		catch(InterruptedException ex) 
+		{
+			Thread.currentThread().interrupt();
+		}
+		
+		if ( !EcoManager.initEco() ) {
+			// Economy nicht eingestellt...
+			this.server.getLogger().log(Level.SEVERE, "The Economy hook couldn't be initialized. Is Vault missing?");
+		}
+		
 	}
 	
 	@Override
@@ -22,7 +41,7 @@ public class UnitedShops extends JavaPlugin {
 
 /*
 	TODO: 
-	- Testbefehl hinzufügen
+	- [DONE] Testbefehl hinzufügen
 	- GUI vervollständigen
 	- EventHandler einstellen
 	- CommandExecutor setzen
