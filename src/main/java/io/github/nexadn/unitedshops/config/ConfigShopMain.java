3f2c95d7e8d43645660018d0535dc225280f6fde
NexAdn <adrianschollmeyer@web.de>
2015-11-11 23:02:01 +0100

    Added HashMap for InventoryID and Configuration key

--- src/main/java/io/github/nexadn/unitedshops/config/ConfigShopMain.java
+++ src/main/java/io/github/nexadn/unitedshops/config/ConfigShopMain.java
@@ -1,9 +1,13 @@
 package io.github.nexadn.unitedshops.config;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class ConfigShopMain extends ConfigBase {
 
+	private HashMap<String,String> categoryList;		// List of categories
+		// <String inventoryId,String categoryKey>
+	
 	public ConfigShopMain(File file) {
 		super(file);
 	}
