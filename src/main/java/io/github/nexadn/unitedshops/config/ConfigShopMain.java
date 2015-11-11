7ac0d71c29d34168972f4c463f4961bd86da2069
NexAdn <adrianschollmeyer@web.de>
2015-11-11 22:52:07 +0100

    Added ConfigShopMain extends ConfigBase

---
+++ src/main/java/io/github/nexadn/unitedshops/config/ConfigShopMain.java
@@ -0,0 +1,11 @@
+package io.github.nexadn.unitedshops.config;
+
+import java.io.File;
+
+public class ConfigShopMain extends ConfigBase {
+
+	public ConfigShopMain(File file) {
+		super(file);
+	}
+	
+}
