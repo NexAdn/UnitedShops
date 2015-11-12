f0da7283eabb75bddf3e21be019602bec30d1d4c
NexAdn <adrianschollmeyer@web.de>
2015-11-12 22:24:13 +0100

    Added menu container, constructors, beginning of paring engine

--- src/main/java/io/github/nexadn/unitedshops/config/ConfigShopMain.java
+++ src/main/java/io/github/nexadn/unitedshops/config/ConfigShopMain.java
@@ -2,15 +2,27 @@ package io.github.nexadn.unitedshops.config;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Set;
+
+import io.github.nexadn.unitedshops.gui.ShopInventory;
 
 public class ConfigShopMain extends ConfigBase {
-
-	private HashMap<String,String> categoryList;		// List of categories
-		// <String inventoryId,String categoryKey>
-	// change to <Integer id, key>
+	HashMap<String,ShopInventory> menus;				// Menu container
 	
 	public ConfigShopMain(File file) {
 		super(file);
 	}
-	
+	public ConfigShopMain(File file, String mainTag)
+	{
+		super(file, mainTag);
+	}
+	// parse the Config File 
+	public void parseConfig()
+	{
+		Set<String> kies = super.getSubKeys(false);
+		for( String s:kies )
+		{
+			this.menus.put(s, new ShopInventory());
+		}
+	}
 }
