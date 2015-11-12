57a9970f28ba1652f1ad57dc86a22886d13eeca9
NexAdn <adrianschollmeyer@web.de>
2015-11-12 19:35:27 +0100

    Added

---
+++ src/main/java/io/github/nexadn/unitedshops/gui/ShopInventory.java
@@ -0,0 +1,5 @@
+package io.github.nexadn.unitedshops.gui;
+
+public class ShopInventory {
+
+}
---
+++ src/main/java/io/github/nexadn/unitedshops/gui/ShopObject.java
@@ -0,0 +1,32 @@
+package io.github.nexadn.unitedshops.gui;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class ShopObject {
+	ItemStack itemstack;			// The ItemStack
+	double pricebuy;				// price to buy an item
+	double pricesell;				// price to sell an item
+	
+	// Constructors
+	public ShopObject(Material material, double buy)
+	{
+		this.itemstack = new ItemStack(material, 1);
+		this.pricebuy = buy;
+		this.pricesell = buy/3;
+	}
+	public ShopObject(Material material, double buy, double sell)
+	{
+		this.itemstack = new ItemStack(material, 1);
+		this.pricebuy = buy;
+		this.pricesell = sell;
+		if(this.pricebuy < this.pricesell/2) {
+			this.pricesell = this.pricebuy/3;
+		}
+	}
+	
+	// getters/setters
+	public ItemStack getItem() { return this.itemstack; }
+	public double getBuy() { return this.pricebuy; }
+	public double getSell() { return this.pricesell; }
+}
