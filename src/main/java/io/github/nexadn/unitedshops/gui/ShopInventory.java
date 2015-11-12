a7c701c41776da81f0d628aaa790ef0acd0f6d0d
NexAdn <adrianschollmeyer@web.de>
2015-11-12 22:26:22 +0100

    Added elements, constructor, initializing engine and getters/setters

--- src/main/java/io/github/nexadn/unitedshops/gui/ShopInventory.java
+++ src/main/java/io/github/nexadn/unitedshops/gui/ShopInventory.java
@@ -1,5 +1,49 @@
 package io.github.nexadn.unitedshops.gui;
 
-public class ShopInventory {
+import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+
+public class ShopInventory {
+	Inventory inv;				// Inventory Holder
+	int order;					// Ordering number
+	String title;				// Inventory title
+	List<ShopObject> content;	// Inventory contents
+	
+	public ShopInventory()
+	{
+		this.order = 0;
+		this.title = "null";
+	}
+	public void initInventory()
+	{
+		int size = content.size();
+		if(size%9!=0)
+		{
+			content.clear();
+			size = 9;
+		}
+		Bukkit.createInventory(null, size, this.title);
+		for(int i=0; i<size; i++)
+		{
+			if(i%45==0)
+			{
+				
+			}
+		}
+	}
+	
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	public void addContent(ShopObject object)
+	{
+		this.content.add(object);
+	}
+	public void setContent(List<ShopObject> contents)
+	{
+		this.content = contents;
+	}
 }
