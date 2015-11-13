622cdc407b46c3ce276dc5b29c8d41d6eeee516f
NexAdn <adrianschollmeyer@web.de>
2015-11-13 18:16:15 +0100

    Added element for "null" objects (non-buyable ones)

--- src/main/java/io/github/nexadn/unitedshops/gui/ShopObject.java
+++ src/main/java/io/github/nexadn/unitedshops/gui/ShopObject.java
@@ -7,6 +7,7 @@ public class ShopObject {
 	ItemStack itemstack;			// The ItemStack
 	double pricebuy;				// price to buy an item
 	double pricesell;				// price to sell an item
+	boolean nobuy = false;			// for placeholder objects: true
 	
 	// Constructors
 	public ShopObject(Material material, double buy)
@@ -24,9 +25,14 @@ public class ShopObject {
 			this.pricesell = this.pricebuy/3;
 		}
 	}
+	public ShopObject()
+	{
+		this.nobuy = true;
+	}
 	
 	// getters/setters
 	public ItemStack getItem() { return this.itemstack; }
 	public double getBuy() { return this.pricebuy; }
 	public double getSell() { return this.pricesell; }
+	public boolean getNobuy() { return this.nobuy; }
 }
