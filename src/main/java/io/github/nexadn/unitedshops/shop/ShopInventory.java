ab2582edbe2b5a0360d54ea2bb08e4082da38378
NexAdn <adrianschollmeyer@web.de>
2015-11-14 22:50:54 +0100

    Added getter for inv, setter for icon

--- src/main/java/io/github/nexadn/unitedshops/shop/ShopInventory.java
+++ src/main/java/io/github/nexadn/unitedshops/shop/ShopInventory.java
@@ -3,6 +3,7 @@ package io.github.nexadn.unitedshops.shop;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -14,6 +15,7 @@ import io.github.nexadn.unitedshops.tradeapi.MoneyTrade;
  */
 public class ShopInventory {
 	Inventory inv;				// Inventory Holder
+	ItemStack icon;				// Icon item
 	int order;					// Ordering number
 	String title;				// Inventory title
 	List<ShopObject> content;	// Inventory contents
@@ -80,6 +82,9 @@ public class ShopInventory {
 	public void setTitle(String title) { this.title = title; }
 	public void addContent(ShopObject object) { this.content.add(object); }
 	public void setContent(List<ShopObject> contents) { this.content = contents; }
+	public void setIcon(Material icon) { this.icon = new ItemStack(icon, 1); }
 	
 	public int getOrderNumber() { return this.order; }
+	public ItemStack getIcon() { return this.icon; }
+	public Inventory getInventory() { return this.inv; }
 }
