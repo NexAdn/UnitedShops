6fa3310fbb61bab513d5a25f40e48a544b0b0758
NexAdn <adrianschollmeyer@web.de>
2015-11-14 19:43:00 +0100

    Added element for ConfigShopMain

--- src/main/java/io/github/nexadn/unitedshops/UnitedShops.java
+++ src/main/java/io/github/nexadn/unitedshops/UnitedShops.java
@@ -6,14 +6,15 @@ import org.bukkit.Server;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import io.github.nexadn.unitedshops.command.ShopGUIHandler;
 import io.github.nexadn.unitedshops.command.UShopDebug;
+import io.github.nexadn.unitedshops.config.ConfigShopMain;
 import io.github.nexadn.unitedshops.tradeapi.EcoManager;
 
 public class UnitedShops extends JavaPlugin {
 	
 	public static Server server;
 	public static FileConfiguration conf;
+	public static ConfigShopMain shopconf;
 	
 	@Override
 	public void onEnable()
