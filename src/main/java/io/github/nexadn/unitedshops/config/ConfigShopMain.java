a4bc3f28921b7d3321e302f0399e7a1c1339e5b1
NexAdn <nexadn@yandex.com>
2017-10-17 18:06:47 +0200

    Version bump 1.0.0
    Signed-off-by: NexAdn <nexadn@yandex.com>
    
    Signed-off-by: NexAdn <nexadn@yandex.com>

--- src/main/java/io/github/nexadn/unitedshops/config/ConfigShopMain.java
+++ src/main/java/io/github/nexadn/unitedshops/config/ConfigShopMain.java
@@ -1,19 +1,3 @@
-/* UnitedShops - A Bukkit 1.8 plugin for shop menus.
-    Copyright (C) 2015 Adrian Schollmeyer
-
-    UnitedShops is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package io.github.nexadn.unitedshops.config;
 
 import java.io.File;
@@ -130,3 +114,21 @@ public class ConfigShopMain extends ConfigBase {
 		return temp;
 	}
 }
+
+/*  Copyright (C) 2015, 2016, 2017 Adrian Schollmeyer
+
+This file is part of UnitedShops.
+
+UnitedShops is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
