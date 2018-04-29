da7ca1771b2dc3605d0e513379c9b71a4d0f45d3
NexAdn <nexadn@yandex.com>
2018-04-29 19:40:16 +0200

    Add test ShopTest dummy

---
+++ src/test/java/io/github/nexadn/unitedshops/ShopTest.java
@@ -0,0 +1,19 @@
+package io.github.nexadn.unitedshops;
+
+import org.junit.Test;
+
+public class ShopTest {
+
+    @Test
+    public void testGui ()
+    {
+        TestUtil.init();
+    }
+
+    @Test
+    public void testTransaction ()
+    {
+        TestUtil.init();
+    }
+
+}
