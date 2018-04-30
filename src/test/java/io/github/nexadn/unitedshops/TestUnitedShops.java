eab1e90a9af136255acea9712eea3f9c606c0051
NexAdn <nexadn@yandex.com>
2018-04-30 11:48:28 +0200

    TestUnitedShops: Make TradeManager a spy

--- src/test/java/io/github/nexadn/unitedshops/TestUnitedShops.java
+++ src/test/java/io/github/nexadn/unitedshops/TestUnitedShops.java
@@ -6,12 +6,14 @@ import java.util.logging.Level;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPluginLoader;
+import org.powermock.api.mockito.PowerMockito;
 
 public class TestUnitedShops extends UnitedShops {
     public TestUnitedShops(JavaPluginLoader mpl, PluginDescriptionFile pdf, File pd, File file)
     {
         super(mpl, pdf, pd, file);
-        this.tradeManager = new TestTradeManager(this);
+        this.tradeManager = PowerMockito.spy(new TestTradeManager(this));
+        UnitedShops.plugin = this;
     }
     
     @Override
