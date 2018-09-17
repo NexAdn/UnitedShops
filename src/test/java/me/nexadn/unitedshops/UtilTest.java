9853551fae5443e6e6b111143d027c182253aa36
NexAdn <nexadn@yandex.com>
2018-09-17 13:55:50 +0200

    Add item type string parser tests to UtilTest

--- src/test/java/me/nexadn/unitedshops/UtilTest.java
+++ src/test/java/me/nexadn/unitedshops/UtilTest.java
@@ -6,12 +6,13 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.junit.Test;
 
+import me.nexadn.unitedshops.exception.InvalidValueException;
 import me.nexadn.unitedshops.testutil.TestUtil;
 
 public class UtilTest {
 
     @Test
-    public void test() {
+    public void getItemTest() {
         TestUtil.init();
 
         ItemStack result = Util.getItem(new Pair<>(Material.STONE, (short) 1), 32, "DISPLAY", "LORE");
@@ -20,6 +21,27 @@ public class UtilTest {
         assertEquals(Material.STONE, result.getType());
         assertEquals("DISPLAY", result.getItemMeta().getDisplayName());
         assertEquals("LORE", result.getItemMeta().getLore().get(0));
+    }
+
+    @Test
+    public void getItemTypeTest() {
+        TestUtil.init();
+
+        Pair<Material, Short> result = Util.parseItemType("STONE:1");
+        assertEquals(Material.STONE, result.first);
+        assertEquals(1, result.second.shortValue());
+
+        result = Util.parseItemType("WOOL:12");
+        assertEquals(Material.WOOL, result.first);
+        assertEquals(12, result.second.shortValue());
+    }
+
+    @Test(expected = InvalidValueException.class)
+    public void getInvalidItemTypeTest() {
+        TestUtil.init();
+
+        // Invalid value should throw
+        Util.parseItemType("STOXXXNE:1");
     }
 
 }
